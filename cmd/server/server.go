39aa7582ef8efb3d9e1b1fa74bceb84b1b1fbcf5
Alpiona <alpiona@hotmail.com>
2022-03-03 11:28:22 -0300

    Added client

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,7 +4,10 @@ import (
 	"log"
 	"net"
 
+	"github.com/alpiona/example-grpc/pb"
+	"github.com/alpiona/example-grpc/services"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -15,6 +18,8 @@ func main() {
 	}
 
 	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, services.NewUserService())
+	reflection.Register(grpcServer)
 
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("Could not serve: %v", err)
